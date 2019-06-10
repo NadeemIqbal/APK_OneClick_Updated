934ad0885a3ddea6e7ee8c7d03db681fef25be79
Nadeem_NGI <nadeem.iqbal>
2019-06-10 12:28:19 +0500

    v4.3 - Apktool and disassemble.cmd updated

--- tools/disassemble.cmd
+++ tools/disassemble.cmd
@@ -10,5 +10,5 @@ SET OUT=%~dpn1-disasm
 cd /d .\apktool
 if exist "%OUT%\." rd /s /q "%OUT%"
 if exist "%USERPROFILE%\apktool\framework\1.apk" del /f "%USERPROFILE%\apktool\framework\1.apk"
-java -Xmx1024m -jar apktool.jar d %APK% "%OUT%"
+java -Xmx1024m -jar apktool.jar d %APK% -f -o "%OUT%"
 if errorlevel 1 echo. &echo An error has occured. &echo. &echo Existing... &pause &exit
