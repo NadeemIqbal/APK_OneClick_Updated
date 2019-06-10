435ec5c85184f91b4e302ca445dc783921e52117
Nadeem_NGI <nadeem.iqbal>
2019-06-10 12:25:30 +0500

    v4.2 original

---
+++ tools/install.cmd
@@ -0,0 +1,13 @@
+@echo off
+color 0a
+cd /d "%~dp0"
+
+SET APK=%1
+
+:: 	Install APK to Phone
+echo Starting to install "%~nx1" to phone
+cd /d .\others
+echo Waiting for device
+adb wait-for-device
+adb install -r %APK%
+if errorlevel 1 echo. &echo An error has occured. &echo. &echo Existing... &pause &exit
